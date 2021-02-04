3fd4c14b1cc8074f479c9b1ba9ed870bcfea50b7
mehdi zanganeh <mehdi.zanganeh@gmail.com>
2021-02-05 00:56:58 +0330

    Initial

---
+++ API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Persistence;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private DataContext _context;
+        public WeatherForecastController(DataContext context)
+        {
+            this._context=context;
+        }
+       
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Value>>> Get()
+        {
+          var values= await _context.Values.ToListAsync();
+          return Ok(values);
+        }
+        
+        [HttpGet("{id}")]
+         public async Task<ActionResult<IEnumerable<Value>>> Get(int id)
+        {
+          var value= await _context.Values.FindAsync(id);
+          return Ok(value);
+        }
+    }
+}
---
+++ Persistence/DataContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+        {
+
+        }
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>().HasData(
+
+                new Value() { Id = 1, Name = "a1" },
+                new Value() { Id = 2, Name = "a2" },
+                new Value() { Id = 3, Name = "a3" }
+            );
+        }
+        public DbSet<Value> Values { get; set; }
+
+
+    }
+}
